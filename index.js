1df3b2959236289780410d21bbe03e67df309419
BellSohn <mcb_box@yahoo.com>
2023-01-15 12:56:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,16 @@ var app = require('./app');
 var port = 3700;
 
 
-//Conexion a la base de datos
-/*27017 es el puerto por defecto de Mongo*/
+//conexion with the db
+/*27017 is the Mongo default port*/
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/barwelt')
 .then(()=>{
-		console.log("database conexion was succesfully stablished...");
-
-			//creacion del servidor
-			app.listen(port,()=>{
-				console.log('server running correctly in the url:localhost:3700');
-			});	
+	console.log("database conexion was succesfully stablished...");
+	//server creation
+	app.listen(port,()=>{
+		console.log('server running correctly in the url:localhost:3700');
+	});	
 
 })
 .catch(err => console.log(err));
